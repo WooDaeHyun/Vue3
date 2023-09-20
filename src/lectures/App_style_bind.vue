661b930b4a2d1dc640fcf8e908112cebb6bc841d
DaeHyun_Woo <po668312@naver.com>
2023-09-20 23:32:25 +0900

    feat: v-if

---
+++ src/lectures/App_style_bind.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-bind:style="styleObject">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab odit soluta tempore minus itaque
+      nesciunt, sequi dolor et, vel placeat reiciendis perferendis possimus, dignissimos fuga alias
+      temporibus. Ipsam, aliquid aut!
+    </div>
+    <button @click="fontSize++">+</button>
+    <button @click="fontSize--">-</button>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+
+export default {
+  setup() {
+    // const styleObject = reactive({
+    //   color: 'red',
+    //   fontSize: '20px'
+    // })
+
+    const fontSize = ref(13)
+
+    const styleObject = computed(() => {
+      return {
+        color: 'blue',
+        fontSize: fontSize.value + 'px'
+      }
+    })
+
+    return { styleObject, fontSize }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/App_v-if.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="visible">Hello vue3!!</div>
+  <button
+    @click="
+      () => {
+        visible = !visible
+      }
+    "
+  >
+    click!
+  </button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const visible = ref(true)
+
+    return { visible }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
