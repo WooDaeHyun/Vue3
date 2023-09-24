c4528a44a0d37f0fd22b6d3da063ac594ccd3ef5
DaeHyun_Woo <po668312@naver.com>
2023-09-24 23:43:15 +0900

    feat: v-model

---
+++ src/lectures/App_v-model.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>input Value</h2>
+    <input type="text" v-model="inputValue" />
+    <!-- :value="inputValue" @input="(event) => (inputValue = event.target.value)" -->
+    <div>{{ inputValue }}</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const inputValue = ref('안녕!')
+
+    return { inputValue }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
