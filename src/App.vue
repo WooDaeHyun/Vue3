6934015fb10828773af322f0c8dee9aa03a0aa1f
DaeHyun_Woo <po668312@naver.com>
2023-09-24 23:07:14 +0900

    feat: event

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <div>
-    <span v-text="msg"></span>
-    <hr />
-    <span>{{ msg }}</span>
-    <hr />
-    <span v-html="strong"></span>
-    <hr />
-    <span v-text="strong"></span>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { ref } from 'vue'
-
 export default {
   setup() {
-    const msg = ref('안녕하세요')
-    const strong = ref('<strong>강조!</strong>')
-
-    return { msg, strong }
+    return {}
   }
 }
 </script>
---
+++ src/lectures/App_event.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="printEvent('메세지', $event)">inline event info</button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const printEvent = (message, event) => {
+      console.log('message', message)
+      console.log('evnetTarget', event.target)
+      console.log('eventTargetTagname', event.target.tagName)
+    }
+
+    return { printEvent }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
