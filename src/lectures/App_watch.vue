2259cdd63ced66938c81f0f9c0ddb8de13c80eaf
DaeHyun_Woo <po668312@naver.com>
2023-09-25 23:35:54 +0900

    feat: bootstrap

---
+++ src/lectures/App_watch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ message }}</div>
+  <input
+    type="text"
+    :value="message"
+    @input="
+      (event) => {
+        message = event.target.value
+      }
+    "
+  />
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+
+export default {
+  setup() {
+    const message = ref('hello')
+
+    watch(message, (newValue, oldValue) => {
+      console.log('뉴', newValue)
+      console.log('올드', oldValue)
+    })
+    return { message }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
