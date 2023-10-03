c8f3350c390a0b902f73bc88cf61f288e1717a1f
DaeHyun_Woo <po668312@naver.com>
2023-10-03 23:17:36 +0900

    feat: props

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,17 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-sm bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-    <div class="container">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
-            <div class="card" style="width: 18rem">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
-                </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card" style="width: 18rem">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
-                </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card" style="width: 18rem">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
-                </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <App_props title="제목" contents="내용" type="타입"></App_props>
+  <App_props :title="post.title" :contents="post.contents" :type="post.type"></App_props>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
-  }
-}
+<script setup lang="ts">
+import { ref } from 'vue'
+import App_props from './lectures/App_props.vue'
+
+const post = ref({
+  title: '제목제목제목제목제목',
+  contents: '내용내용내용내용내용내용',
+  type: 'news'
+})
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/lectures/App_props.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span class="badge bg-secondary">{{ type === 'news' ? '뉴스' : '공지사항' }}</span>
+    <div>{{ title }}</div>
+    <p>{{ contents }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'news'
+    },
+    contents: {
+      type: String,
+      default: '내용!'
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
